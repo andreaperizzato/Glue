0389d254eef0a7385d2c828fcfdd6a2cddaa819a
AndreaPerizzato <andrea.perizzato@digital.well.co.uk>
2019-05-13 17:01:54 +0100

    Lowered min deployment version

--- Glue/Glue.h
+++ Glue/Glue.h
@@ -6,14 +6,12 @@
 //  Copyright © 2019 Andrea Perizzato. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for Glue.
 FOUNDATION_EXPORT double GlueVersionNumber;
 
 //! Project version string for Glue.
 FOUNDATION_EXPORT const unsigned char GlueVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Glue/PublicHeader.h>
 
 
